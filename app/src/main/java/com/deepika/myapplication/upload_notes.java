76fe71fcc4a8a399067b5339e4777c401049a548
Chethan S <cchethans14@gmail.com>
2021-07-04 16:22:07 +0530

    1

--- app/src/main/java/com/deepika/myapplication/upload_notes.java
+++ app/src/main/java/com/deepika/myapplication/upload_notes.java
@@ -3,6 +3,11 @@ package com.deepika.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
 
 public class upload_notes extends AppCompatActivity {
 
@@ -10,5 +15,43 @@ public class upload_notes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_notes);
+        initUI();
+    }
+
+    private void initUI() {
+        //UI reference of textView
+        final AutoCompleteTextView customerAutoTV = findViewById(R.id.customerTextView);
+
+        // create list of customer
+        ArrayList<String> customerList = getCustomerList();
+
+        //Create adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(upload_notes.this, R.layout.spinner_item, customerList);
+
+        //Set adapter
+        customerAutoTV.setAdapter(adapter);
+
+        //submit button click event registration
+//        findViewById(R.id.submitButton).setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Toast.makeText(MainActivity.this, customerAutoTV.getText(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+
+
+    private ArrayList<String> getCustomerList()
+    {
+        ArrayList<String> customers = new ArrayList<>();
+        customers.add("Kannada");
+        customers.add("English");
+        customers.add("Hindi");
+        customers.add("Mathematics");
+        customers.add("Science");
+        customers.add("Social");
+        return customers;
     }
 }
